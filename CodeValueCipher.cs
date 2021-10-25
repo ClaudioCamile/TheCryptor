5255b6bc1e3c2cb2cd9e7cdc05048ce92398f3ee
Innovation & Development <camileclaudio@gmail.com>
2021-10-25 03:45:35 +0300

    Add files via upload

---
+++ CodeValueCipher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cryptor
+{
+    public class CodeValueCipher : ICodeValueCipher
+    {
+        public string Encrypt(string message, string key)
+        {
+            string encrypt = CryptIt(message, key, false);
+            return encrypt;
+        }
+        public string Decrypt(string message, string key)
+        {
+            string decrypt = CryptIt(message, key, true);
+            return decrypt;
+        }
+
+        private string CryptIt(string message, string key, bool action)
+        {
+            string userErrorNotification = "Input have to be only letters or spaces, please correct your input and try again";
+            Regex rx = new Regex(("^[a-zA-Z ]*$"));
+            if (!rx.IsMatch(message)) return userErrorNotification;
+
+            string orderedKey = "ABCDEFGHIJKLMNOPQRSTVUWXYZ";
+            string unOrderedKey = key;
+            key = action ? orderedKey : unOrderedKey;
+
+            var sb = new StringBuilder("", 26);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (key.IndexOf(message[i]) >= 0)
+                {
+                    int idx = key.IndexOf(message[i]);
+                    Char lookFor = key[idx];
+
+                    int dx = action ? 
+                        unOrderedKey.IndexOf(lookFor) : 
+                        orderedKey.IndexOf(lookFor);
+
+                    var correspond = key[dx];
+
+                    var tmp = (Char.IsLetter(lookFor) && Char.IsUpper(lookFor)) ?
+                        Char.ToUpper(correspond) :
+                            Char.IsLetter(lookFor) ? Char.ToLower(correspond) : lookFor;
+                    sb.Append(tmp);
+                }
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cryptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                    
+            string encryptionkey = "JIECHSDUGFRVAWNQTYBZOLKMPX";
+            string messageToEncript = "ABC";
+
+            CodeValueCipher cvc = new CodeValueCipher();
+
+            // expect JIE
+            string encryptedResult = cvc.Encrypt(messageToEncript, encryptionkey);
+            Console.WriteLine("The message " + messageToEncript  + " Encrypted to: " + encryptedResult);
+
+            // expect ABC
+            messageToEncript = encryptedResult;
+            encryptedResult = cvc.Decrypt(messageToEncript, encryptionkey);
+            Console.WriteLine("The message " + messageToEncript + " Decrypted to: " + encryptedResult);
+
+        }
+    }
+}
